3c7d01090128bef9b3b0a1b1ca8310ba78818158
frosekhan-05 <frosekhan05@gmail.com>
2026-03-11 10:32:46 +0530

    neetcodeproblem

---
+++ IsSubsequence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i=0;int j=0;
+        while(i<s.length()&&j<t.length()){
+             if(s.charAt(i) == t.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+        return i==s.length();
+    }
+}// check this link:https://neetcode.io/problems/is-subsequence/question
