2550c5479ffdd4a961c06def2f8746c3f1d21ce6
frosekhan-05 <frosekhan05@gmail.com>
2026-03-11 10:43:32 +0530

    neetcodeprogram

---
+++ MissinginArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    int missingNum(int arr[]) {
+        // code here
+        int n=arr.length+1;
+        int freq[]=new int[n+1];
+        for(int i:arr){
+            freq[i]++;
+        }
+        for(int i=1;i<=n;i++){
+            if(freq[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+}// check the link here:https://www.geeksforgeeks.org/problems/missing-number-in-array1416/1?page=1&category=Arrays,Java&sortBy=submissions
